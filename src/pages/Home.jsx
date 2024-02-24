fc5704cc799472149b33061b4dfb3c3913924b47
srisaisnigdha <98080072+srisaisnigdha@users.noreply.github.com>
2024-02-24 15:28:47 +0530

    added text-box in home page for searching, search for shows on button click

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+
 const Home = () => {
     // return (<div>Home Page</div>)
+
+    const [SearchStr, setSearchStr] = useState(""); //passing empty string as default value
+
+    const onSearchInputChange = (ev) => {
+        setSearchStr(ev.target.value);
+    }
+
+    const onSearch = async (ev) => {
+        ev.preventDefault();   // we can prevent the default behaviour of this event
+
+        // fetch("https://api.tvmaze.com/search/shows?q=girls")
+        // .then(response=>response.json())
+        // .then(body=>console.log(body))     ... without using async function
+
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${SearchStr}`)   // made the request as dynamic using ${}
+        const body = await response.json();
+        console.log(body)
+    }
+
     return (
         <div>
-            Home
+            <form onSubmit={onSearch}>
+                {/* event handler for onSubmit is onSearch */}
+                <input type="text" value={SearchStr} onChange={onSearchInputChange} />
+                {/* event handler for onChange is onSearchInputChange */}
+                <button type="submit">Search</button>
+            </form>
         </div>
     )
 }
