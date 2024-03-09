dcfb6d03be4caaa7395bc447ae752d710b0e6760
srisaisnigdha <98080072+srisaisnigdha@users.noreply.github.com>
2024-03-09 13:19:48 +0530

    created useStarredShows custom hook. placed useStarredShows hook logic in a separate file. display user whether show is starred or not

--- src/components/shows/ShowGrid.jsx
+++ src/components/shows/ShowGrid.jsx
@@ -1,36 +1,9 @@
-import { useEffect, useReducer } from "react";
 import ShowCard from "./ShowCard";
+import { useStarredShows } from "../../lib/useStarredShows";
 
-const usePersistedReducer = (reducer, initialState, localStorageKey) => {
-    const [state, dispatch] = useReducer(reducer, initialState, (initial) => {
-        const persistedValue = localStorage.getItem(localStorageKey);
-
-        // local storage always works only with strings, means 'persistedValue' here is a string
-        return persistedValue ? JSON.parse(persistedValue) : initial;
-    });
-
-    useEffect(() => {
-        localStorage.setItem(localStorageKey, JSON.stringify(state))
-    }, [state, localStorageKey]);
-
-    return [state, dispatch]
-}
-
-const starredShowsReducer = (currentStarred, action) => {
-    switch (action.type) {
-        case 'STAR':
-            return currentStarred.concat(action.showId);
-        case 'UNSTAR':
-            return currentStarred.filter((showId) => showId !== action.showId);
-        default:
-            return currentStarred
-    }
-}
 
 const ShowGrid = ({ shows }) => {
-
-    const [starredShows, dispatchStarred] = usePersistedReducer(starredShowsReducer, [], 'starredShows');
-    // console.log({ starredShows })
+    const [starredShows, dispatchStarred] = useStarredShows();
 
     const onStarMeClick = (showId) => {
         const isStarred = starredShows.includes(showId);
@@ -54,6 +27,7 @@ const ShowGrid = ({ shows }) => {
                     summary={data.show.summary}
 
                     onStarMeClick={onStarMeClick}
+                    isStarred={starredShows.includes(data.show.id)}
                 />)
         }
     </div>
--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,5 +1,9 @@
-const Starred=()=>{
-    return <div>Starred Page</div>
+import { useStarredShows } from "../lib/useStarredShows";
+
+const Starred = () => {
+    const [starredShows] = useStarredShows();
+
+    return <div>Starred Page, starred {starredShows.length}</div>
 }
 
 export default Starred;
