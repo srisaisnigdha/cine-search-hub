affb02c779899309c52500ea3923f229d5055596
srisaisnigdha <98080072+srisaisnigdha@users.noreply.github.com>
2024-03-11 23:43:55 +0530

    fetched starred shows data and display ShowGrid for them

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -9,5 +9,12 @@ const apiGet = async queryString => {
 
 export const searchForShows = query => apiGet(`/search/shows?q=${query}`); // it is a reusable function to query for shows endpoint
 export const searchForPeople = query => apiGet(`/search/people?q=${query}`); // it is a reusable function to query for actors endpoint
+
 export const getShowById = showId =>
   apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`);
+
+export const getShowsByIds = async showIds => {
+  const promises = showIds.map(showId => apiGet(`/shows/${showId}`)); // array of promises. getShowById(showId) - is a promise
+  const result = await Promise.all(promises); // all the promises are resolved in parallel
+  return result; // result = array of shows
+};
--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,9 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
 import { useStarredShows } from "../lib/useStarredShows";
+import { getShowsByIds } from "../api/tvmaze";
+import ShowGrid from "../components/shows/ShowGrid";
 
 const Starred = () => {
-    const [starredShows] = useStarredShows();
+    const [starredShowsIds] = useStarredShows();
 
-    return <div>Starred Page, starred {starredShows.length}</div>
+    const { data: starredShows, error: starredShowsError } = useQuery({
+        queryKey: ['starred', starredShowsIds],
+        queryFn: () => getShowsByIds(starredShowsIds).then(result =>
+            result.map(show => ({ show }))
+            // nested the shows data that we received from getShowsByIds inside 'show' keyword, so that it matches with the logic structure present in the showGrid to display
+        ),
+        refetchOnWindowFocus: false
+    });
+
+    // applying conditional redering to display data
+    if (starredShows?.length === 0) {
+        return <div>No shows were starred</div>
+    }
+
+    // if (starredShows && starredShows.length > 0)   ==  if (starredShows?.length > 0) 
+    if (starredShows?.length > 0) {
+        return <ShowGrid shows={starredShows} />
+    }
+
+    if (starredShowsError) {
+        return <div>Error occured: {starredShowsError.message} </div>
+    }
+
+    return <div>Shows are loading</div>
 }
 
 export default Starred;
